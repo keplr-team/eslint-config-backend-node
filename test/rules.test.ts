f50cbe3bc5c9979c1108bb68485c1a2be1e7841c
Nicolas Even <nicolas.even@gokepler.fr>
2022-07-28 16:54:11 +0200

    Re-add the tests + bump everything + add a few rules that look useful

--- test/rules.test.ts
+++ test/rules.test.ts
@@ -1,28 +1,38 @@
-// Test commented while waiting for the removed getRules() function to be replaced
+import { ESLint, Linter } from 'eslint';
+import * as _ from 'lodash';
+import { rules as tsRules } from '@typescript-eslint/eslint-plugin';
+//@ts-expect-error
+import { rules as importRules } from 'eslint-plugin-import';
+//@ts-expect-error
+import { rules as jestRules } from 'eslint-plugin-jest';
 
-// import { CLIEngine } from 'eslint';
-// import * as _ from 'lodash';
+describe('ESLint rules', () => {
+  it.each(['js', 'ts', 'test.js', 'test.ts', 'e2e.js', 'e2e.ts'])(
+    'should match defined rules for %s',
+    async ext => {
+      const engine = new ESLint({ cwd: `${__dirname}/..` });
+      const fileConfig = await engine.calculateConfigForFile(`index.${ext}`);
+      expect(fileConfig).toMatchSnapshot(
+        {
+          parser: expect.stringMatching(
+            'node_modules/@typescript-eslint/parser/dist/index.js',
+          ),
+        },
+        'Applied rules',
+      );
+      const rulesList = [
+        Array.from(new Linter().getRules().keys()),
+        Object.keys(tsRules).map(r => `@typescript-eslint/${r}`),
+        // eslint-disable-next-line @typescript-eslint/no-unsafe-argument
+        Object.keys(importRules).map(r => `import/${r}`),
+        // eslint-disable-next-line @typescript-eslint/no-unsafe-argument
+        Object.keys(jestRules).map(r => `jest/${r}`),
+      ].flat(1);
 
-// const configPath = require.resolve('../');
-
-// describe('ESLint rules', () => {
-//   it.skip.each(['js', 'ts', 'test.js', 'test.ts', 'e2e.js', 'e2e.ts'])(
-//     'should match defined rules for %s',
-//     ext => {
-//       const engine = new CLIEngine({ configFile: configPath });
-//       const fileConfig = engine.getConfigForFile(`index.${ext}`);
-//       expect(fileConfig).toMatchSnapshot(
-//         {
-//           parser: expect.stringMatching(
-//             'node_modules/@typescript-eslint/parser/dist/index.js',
-//           ),
-//         },
-//         'Applied rules',
-//       );
-//       const rules = engine.getRules();
-//       expect(
-//         _.difference(Array.from(rules.keys()), Object.keys(fileConfig.rules!)),
-//       ).toMatchSnapshot('Ignored rules');
-//     },
-//   );
-// });
+      expect(
+        // eslint-disable-next-line @typescript-eslint/no-unsafe-argument, @typescript-eslint/no-unsafe-member-access
+        _.difference(rulesList, Object.keys(fileConfig.rules!)),
+      ).toMatchSnapshot('Ignored rules');
+    },
+  );
+});
