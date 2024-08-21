c81aa17165ce3b04707640011005e0ba42953ca0
Pierre SAVIGNY <27277903+Arkanheal@users.noreply.github.com>
2024-08-21 10:35:30 +0200

    Properly type fileConfig and remove lodash

--- test/rules.test.ts
+++ test/rules.test.ts
@@ -1,17 +1,30 @@
 import { ESLint, Linter } from 'eslint';
-import * as _ from 'lodash';
 import { rules as tsRules } from '@typescript-eslint/eslint-plugin';
 //@ts-expect-error
 import { rules as importRules } from 'eslint-plugin-import';
 //@ts-expect-error
 import { rules as jestRules } from 'eslint-plugin-jest';
 
+interface IFileConfig {
+  [key: string]: unknown;
+  rules: {
+    [key: string]: unknown;
+  };
+  parser: string;
+}
+
+function difference(a: string[], b: string[]): string[] {
+  return a.filter(elA => !b.includes(elA));
+}
+
 describe('ESLint rules', () => {
   it.each(['js', 'ts', 'test.js', 'test.ts', 'e2e.js', 'e2e.ts'])(
     'should match defined rules for %s',
     async ext => {
       const engine = new ESLint({ cwd: `${__dirname}/..` });
-      const fileConfig = await engine.calculateConfigForFile(`index.${ext}`);
+      const fileConfig: IFileConfig = await engine.calculateConfigForFile(
+        `index.${ext}`,
+      );
       expect(fileConfig).toMatchSnapshot(
         {
           parser: expect.stringMatching(
@@ -30,8 +43,7 @@ describe('ESLint rules', () => {
       ].flat(1);
 
       expect(
-        // eslint-disable-next-line @typescript-eslint/no-unsafe-argument, @typescript-eslint/no-unsafe-member-access
-        _.difference(rulesList, Object.keys(fileConfig.rules!)),
+        difference(rulesList, Object.keys(fileConfig.rules)),
       ).toMatchSnapshot('Ignored rules');
     },
   );
